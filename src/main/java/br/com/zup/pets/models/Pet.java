e31500b6961151928c9d4ffddd5c0ac09df18bf6
Iris <iris.souza@zup.com.br>
2021-03-09 16:13:00 -0300

    Inicira projeto

---
+++ src/main/java/br/com/zup/pets/models/Pet.java
@@ -0,0 +1,14 @@
+package br.com.zup.pets.models;
+
+import java.time.LocalDate;
+
+public class Pet {
+    private String nome;
+    private String raca;
+    private String especie;
+    private LocalDate dataDeEntrada;
+    private String nomeDoDono;
+    private String emailDoDono;
+    private String telefoneDoDono;
+
+}
