9df7bcecd08b0422d965288a7974fc4fb0aa75b0
Iris <iris.souza@zup.com.br>
2021-03-10 12:22:05 -0300

    Adicionar classe ObjetoDeErro

--- src/main/java/br/com/zup/pets/exceptions/ObjetoDeErro.java
+++ src/main/java/br/com/zup/pets/exceptions/ObjetoDeErro.java
@@ -1,4 +1,31 @@
 package br.com.zup.pets.exceptions;
 
 public class ObjetoDeErro {
+    private String mensagem;
+    private String campoDoErro;
+
+    public ObjetoDeErro() {
+    }
+
+    public ObjetoDeErro(String mensagem, String campoDoErro) {
+        this.mensagem = mensagem;
+        this.campoDoErro = campoDoErro;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getCampoDoErro() {
+        return campoDoErro;
+    }
+
+    public void setCampoDoErro(String campoDoErro) {
+        this.campoDoErro = campoDoErro;
+    }
 }
+
